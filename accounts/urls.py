db28a75330a684c3a9d2bee1fcaed3ff0f927dad
Karma Gurung <63505288+KarmaGurung75@users.noreply.github.com>
2022-02-01 09:11:54 +0545

    homepage

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage),
+    path('login', views.login_user),
+    path('register', views.register_user),
+    path('logout', views.logout_user),
+    path('my_profile', views.profile),
+    path('edit_profile', views.edit_profile),
+    path('password_change', views.change_password),
+]
